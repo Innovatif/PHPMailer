55d947cdd823f24c31fa2ab12ae07a25852d6dda
miiihi <blaz@innovatif.com>
2013-10-08 21:12:55 +0200

    DEBUG...

--- class.smtp.php
+++ class.smtp.php
@@ -154,6 +154,8 @@ class SMTP
      */
     protected function edebug($str)
     {
+    	$str = getmypid() .' '. $str;
+    	
         switch ($this->Debugoutput) {
             case 'error_log':
                 //Don't output, just log
